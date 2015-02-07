367afd11d76b88061953bd947d570dea37a32917
vetterm <Marcmaurice@web.de>
2015-02-07 21:01:11 +0000

    mbti-list

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import json
 from models.User import User
 from controller.UserController import UserController
-import this
+#import this
 from nlp.LogLinearClassifier import LinearClassifier
 
 def main():
-  mbti_type = "intj"
-  uc = UserController()
-  user_ids = uc.getUsersBySearchTerm("mbti " + mbti_type)
-
-  for id in user_ids:
-    user = User(id, mbti_type)
+	mbti_types_list=['INFP','INFJ','INTJ','INTP','ISFJ','ISFP','ISTJ','ISTP','ENFJ','ENFP','ENTJ','ENTP','ESFJ','ESFP','ESTJ','ESTP'];
+	
+	print(mbti_types_list)
 
 
-  #   Log Linear Model
-  LLC = LinearClassifier()
-  LLC.trainArguments(docs)
-  LLC.perceptron(docs, 10)
-
+	for mbti_type in mbti_types_list:
+		uc = UserController()
+		user_ids = uc.getUsersBySearchTerm("mbti " + mbti_type)
+	
+		for id in user_ids:
+	    		user = User(id, mbti_type)
+	
+	
+	  #   Log Linear Model
+	#  LLC = LinearClassifier()
+	#  LLC.trainArguments(docs)
+	#  LLC.perceptron(docs, 10)
+	
 
 if __name__ == '__main__':
   main()
+
 
