91806c7d97922d629b4b899fba5dc64809b91e64
Catalina Predoi <catalinapre2@yahoo.co.uk>
2015-02-08 12:19:52 +0000

    Server code - stil lcontains bugs

---
+++ server/server/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = patterns('',
+    url(r'^$', 'server.views.home', name='home'),
+    url(r'^getfollowerpersonality/', 'server.views.handleProcess', name='home'),
+    url(r'^results/', 'server.views.results', name='home'),
+    # Examples:
+    # url(r'^$', 'server.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
