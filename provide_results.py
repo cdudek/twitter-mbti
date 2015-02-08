839f2cf729726ceef60b3b9135b03e0c3590c1e5
vetterm <Marcmaurice@web.de>
2015-02-08 13:43:22 +0000

    provide_results
    outputs: x times higher ratio of mbti-type than baseline

--- provide_results.py
+++ provide_results.py
@@ -11,4 +11,20 @@ def get_follower_types(name):
     else:
         return {}
 
-print get_follower_types("bmw")
+
+def account_for_baseline(data):
+    mbti_twitter_baseline={'INFP': 0.069, 'INFJ': 0.037, 'INTJ': 0.048, 'INTP': 0.059, 'ISFJ': 0.032, 'ISFP': 0.016, 'ISTJ': 0.101, 'ISTP': 0.048, 'ENFJ': 0.08, 'ENFP': 0.106, 'ENTJ': 0.085, 'ENTP': 0.117, 'ESFJ': 0.021, 'ESFP': 0.016, 'ESTJ': 0.112, 'ESTP': 0.053}
+    count=data
+    total=0
+    for type in data:
+        n=len(count[type])
+        total=total+n
+        count[type]=n
+    distribution = count
+    for x in distribution:
+        distribution[x]=round(count[x] / float(total), 4)
+    distribution_factor=[round(distribution[x] / float(mbti_twitter_baseline[x]), 4) for x in distribution]
+    print distribution_factor
+
+
+account_for_baseline(get_follower_types("bmw"))
